f6df0748440d8c700d7c5d7f541d09ab84cad411
Arthur <arthurszaa7@icloud.com>
2025-07-27 23:23:08 -0300

    new: Enumeracoes Enums

--- src/JavaJiraya/JavaCore/Kenum/teste/ClienteTeste01.java
+++ src/JavaJiraya/JavaCore/Kenum/teste/ClienteTeste01.java
@@ -1,16 +1,21 @@
 package JavaJiraya.JavaCore.Kenum.teste;
 
 import JavaJiraya.JavaCore.Kenum.dominio.Cliente;
-import JavaJiraya.JavaCore.Kenum.dominio.Cliente.TipoPagamento;
 import JavaJiraya.JavaCore.Kenum.dominio.TipoCliente;
+import JavaJiraya.JavaCore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTeste01 {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Arthur", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
         Cliente cliente2 = new Cliente("Bruna", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
+
         System.out.println(cliente1);
         System.out.println(cliente2);
-
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente);
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
     }
 }
